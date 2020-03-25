f1b63f2abeb867319a7512e39e0f09f137921422
xujunmeng.github <xujunmeng2012@163.com>
2020-03-25 11:08:20 +0800

    update

--- src/main/java/com/SpringRpcDemoApplication.java
+++ src/main/java/com/SpringRpcDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.remoting.rmi.RmiProxyFactoryBean;
+import org.springframework.remoting.caucho.HessianProxyFactoryBean;
 
 /**
  * @author james
@@ -14,18 +14,29 @@ import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 @SpringBootApplication
 public class SpringRpcDemoApplication {
 
+//    /**
+//     * 客户端调用远程服务
+//     * @return
+//     */
+//    @Bean
+//    public RmiProxyFactoryBean userRmiServiceProxy() {
+//        RmiProxyFactoryBean rmiProxyFactoryBean = new RmiProxyFactoryBean();
+//        //接收的rmi协议
+//        rmiProxyFactoryBean.setServiceUrl("rmi://127.0.0.1:1199/userRmiService");
+//        //接收的rmi协议的接口
+//        rmiProxyFactoryBean.setServiceInterface(IUserService.class);
+//        return rmiProxyFactoryBean;
+//    }
+
     /**
      * 客户端调用远程服务
-     * @return
      */
     @Bean
-    public RmiProxyFactoryBean userRmiServiceProxy() {
-        RmiProxyFactoryBean rmiProxyFactoryBean = new RmiProxyFactoryBean();
-        //接收的rmi协议
-        rmiProxyFactoryBean.setServiceUrl("rmi://127.0.0.1:1199/userRmiService");
-        //接收的rmi协议的接口
-        rmiProxyFactoryBean.setServiceInterface(IUserService.class);
-        return rmiProxyFactoryBean;
+    public HessianProxyFactoryBean userHessianServiceProxy() {
+        HessianProxyFactoryBean hessianProxyFactoryBean = new HessianProxyFactoryBean();
+        hessianProxyFactoryBean.setServiceUrl("http://127.0.0.1:8022/userHessianService");
+        hessianProxyFactoryBean.setServiceInterface(IUserService.class);
+        return hessianProxyFactoryBean;
     }
 
     public static void main(String[] args) {
